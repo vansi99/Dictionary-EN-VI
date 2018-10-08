8e5c29bb82a189f130270c33b685d8558074137a
vansi99 <vansido99@gmail.com>
2018-10-08 12:45:24 +0700

    change

--- dictionaryApp/src/sample/Main.java
+++ dictionaryApp/src/sample/Main.java
@@ -6,10 +6,17 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.ResultSet;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        DbWords words = new DbWords();
+        ResultSet rs = words.getDetails();
+        if(rs.next()) {
+            System.out.println("nghia cua tu la:" + rs.getString("detail"));
+        }
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
