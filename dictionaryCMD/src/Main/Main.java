f572652411eaf69aa497ab10d0d569abfed0a5b2
HongHanh120 <lehonghanh.120@gmail.com>
2018-10-13 23:17:52 +0700

    ok

--- dictionaryCMD/src/Main/Main.java
+++ dictionaryCMD/src/Main/Main.java
@@ -1,10 +1,11 @@
+package Main;
 
 import Model.DbWords;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nhập phím 1: TRA TỪ" + "\nNhập phím 2: THÊM TỪ" + "\nNhập phím 3: THOÁT KHỎI CHƯƠNG TRÌNH");
+        System.out.println("Nhập phím 1: TRA TỪ" + "\nNhập phím 2: THÊM TỪ" + "\nNhập phím 4: XÓA TỪ" + "\nNhập phím 4: THOÁT KHỎI CHƯƠNG TRÌNH");
         boolean running = true;
         while(running) {
             Scanner scannerNumber = new Scanner(System.in);
@@ -19,16 +20,21 @@ public class Main {
                     System.out.println("Trong từ điển hiện không có từ này!");
                 System.out.println("\nNghĩa của từ là: " + detail);
             }
-            if(number == 2) {
+            else if(number == 2) {
                 Scanner scanner = new Scanner(System.in);
                 System.out.print("Nhập từ muốn thêm: ");
-                String word = scanner.nextLine().toLowerCase();
+                String word = scanner.nextLine();
                 System.out.print("Nhập nghĩa của từ muốn thêm: ");
                 String detail = scanner.nextLine().toLowerCase();
                 DbWords postWord = new DbWords();
                 postWord.postWord(word, detail);
             }
-            if(number == 3){
+            else if(number == 3){
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Nhập từ muốn thêm: ");
+                String word = scanner.nextLine();
+            }
+            else if(number == 4){
                 running = false;
             }
         }
