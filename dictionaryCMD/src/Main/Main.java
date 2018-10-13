683fe6a5751c2c62bb9ea201b887117315950c0e
HongHanh120 <lehonghanh.120@gmail.com>
2018-10-13 13:02:03 +0700

    add

--- dictionaryCMD/src/Main/Main.java
+++ dictionaryCMD/src/Main/Main.java
@@ -7,19 +7,25 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập từ cần tra: ");
-        String word = scanner.nextLine().toLowerCase();
-        DbWords words = new DbWords(word);
-        try {
-            ResultSet rs = words.getDetail();
-            if(rs.next()) {
-                System.out.println("\nNghia của từ là: " + rs.getString("detail"));
-            } else System.out.println("\n:v Từ điển hiện không có từ này!!");
-        } catch (SQLException e){
-            e.printStackTrace();
+        while(true) {
+            System.out.println("Nhập phím 1: Tra từ" + "\nNhập phím 2: Thêm từ");
+            Scanner scanner1 = new Scanner(System.in);
+            int number = scanner1.nextInt();
+            if (number == 1) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Nhập từ cần tra: ");
+                String word = scanner.nextLine().toLowerCase();
+                DbWords words = new DbWords(word);
+                try {
+                    ResultSet rs = words.getDetail();
+                    if (rs.next()) {
+                        System.out.println("\nNghĩa của từ là: " + rs.getString("detail"));
+                    } else System.out.println("\nTừ điển hiện không có từ này!!!");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
