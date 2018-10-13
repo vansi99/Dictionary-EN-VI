97cd7647b376f86339d128460faf8c4dd94f47c0
HongHanh120 <lehonghanh.120@gmail.com>
2018-10-13 18:53:09 +0700

    add

--- dictionaryCMD/src/Main/Main.java
+++ dictionaryCMD/src/Main/Main.java
@@ -8,10 +8,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Nhập phím 1: TRA TỪ" + "\nNhập phím 2: THÊM TỪ" + "\nNhập phím 3: XÓA TỪ");
         while(true) {
-            System.out.println("Nhập phím 1: Tra từ" + "\nNhập phím 2: Thêm từ");
-            Scanner scanner1 = new Scanner(System.in);
-            int number = scanner1.nextInt();
+            Scanner scannerNumber = new Scanner(System.in);
+            int number = scannerNumber.nextInt();
             if (number == 1) {
                 Scanner scanner = new Scanner(System.in);
                 System.out.print("Nhập từ cần tra: ");
@@ -21,10 +21,21 @@ public class Main {
                     ResultSet rs = words.getDetail();
                     if (rs.next()) {
                         System.out.println("\nNghĩa của từ là: " + rs.getString("detail"));
-                    } else System.out.println("\nTừ điển hiện không có từ này!!!");
-                } catch (SQLException e) {
+                    }
+                    else System.out.println("\nTừ điển hiện không có từ này!!!");
+                }
+                catch (SQLException e) {
                     e.printStackTrace();
                 }
+            }
+            if(number == 2){
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Nhập từ muốn thêm: ");
+                String word = scanner.nextLine().toLowerCase();
+                System.out.print("Nhập nghĩa của từ muốn thêm: ");
+                String detail = scanner.nextLine().toLowerCase();
+                DbWords postWord = new DbWords();
+                postWord.postWord(word, detail);
             }
         }
     }
