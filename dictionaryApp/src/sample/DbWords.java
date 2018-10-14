44eec1fd056c939bba77365c7c69b2d5b217c3c2
vansi99 <vansido99@gmail.com>
2018-10-14 17:35:25 +0700

    ok

--- dictionaryApp/src/sample/DbWords.java
+++
@@ -1,8 +0,0 @@
-package sample;
-import java.sql.*;
-
-public class DbWords extends Db{
-    public ResultSet getDetails(String word){
-        return getData("SELECT detail FROM my_db WHERE word = ?",word);
-    }
-}
