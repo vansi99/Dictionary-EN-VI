b870ca0f5745fc8b24abff6267ec70fb807003dc
HongHanh120 <lehonghanh.120@gmail.com>
2018-10-20 12:48:48 +0700

    app

--- dictionaryApp/src/View/Main.java
+++ dictionaryApp/src/View/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
         primaryStage.setTitle("Dictionary");
         primaryStage.setScene(new Scene(root, 700, 450));
         primaryStage.show();
--- dictionaryApp/src/controller/Controller.java
+++ dictionaryApp/src/controller/Controller.java
@@ -1,6 +1,9 @@
 package controller;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import model.Db;
@@ -14,15 +17,23 @@ public class Controller {
 
 
     public WebView browser;
-
     public Button button_search;
+    public TextField get_word;
+    public ListView<String> referable_list ;
 
-    public void handleOnSearch() {
-        System.out.println("si ngu si");
+    public void handleOnSearch(){
+        WebEngine webEngine = browser.getEngine();
+        String text = get_word.getText();
+        get_word.setText(text);
+        System.out.println(text);
+        String html = "<html><i>zymurgy</i><br/><ul><li><b><i> danh từ</i></b><ul><li><font color='#cc0000'><b> khoa nghiên cứu sự lên men</b></font></li></ul></li></ul></html>";
+        webEngine.loadContent(html);
     }
 
-
-
+    public void showListView(){
+        referable_list.getItems().addAll("Iron Man", "Harry Potter", "Percy Jackson");
+        referable_list.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+    }
 
 
 
@@ -38,14 +49,13 @@ public class Controller {
 //        System.out.println("si dz nhat!");
 //        WebEngine webEngine = browser.getEngine();
 //        DbWords words = new DbWords();
-//
 //        words.setWord("word");
 //        try {
 //            ResultSet rs = words.getDetail();
 //            while (rs.next()){
 //                System.out.println(rs.getString("detail"));
 //                String detail = rs.getString("detail");
-//                String html = detail ;
+//                String html = detail;
 //                webEngine.loadContent(html);
 //            }
 //        } catch (SQLException e){
